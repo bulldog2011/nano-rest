1071d6de7e772a270b960e1668f1213a556c4bfa
bulldog2011 <51startup@sina.com>
2013-02-19 20:45:17 +0800

    ebay search sample added

---
+++ sample/EBaySearch/src/com/ebay/finding/FindingService.java
@@ -0,0 +1,19 @@
+package com.ebay.finding;
+
+import com.ebay.finding.request.BaseFindingRequestProcessor;
+import com.ebay.marketplace.search.v1.services.FindItemsByKeywordsRequest;
+import com.ebay.marketplace.search.v1.services.FindItemsByKeywordsResponse;
+import com.leansoft.nanorest.RequestProcessor;
+import com.leansoft.nanorest.callback.HttpCallback;
+
+public class FindingService {
+	
+	public static final String TAG = FindingService.class.getSimpleName();
+	
+	public static RequestProcessor getFindItemsByKeywordsHttpRequest( 
+			FindItemsByKeywordsRequest requestObject, 
+			HttpCallback<FindItemsByKeywordsResponse> callback) {
+		return new BaseFindingRequestProcessor<FindItemsByKeywordsResponse>(requestObject, "findItemsByKeywords", FindItemsByKeywordsResponse.class, callback);
+	}
+
+}
